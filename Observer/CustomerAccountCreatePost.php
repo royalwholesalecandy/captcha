de424ed9d7e21ee17960eab1faca74dbbb8208d4
Dmitry Fedyuk <dfediuk@gmail.com>
2019-11-21 06:14:25 +0600

    0.0.5

--- Observer/CustomerAccountCreatePost.php
+++ Observer/CustomerAccountCreatePost.php
@@ -4,11 +4,11 @@
  * @used-by \Magento\Framework\App\Action\Action::dispatch():
  *	$this->_eventManager->dispatch('controller_action_predispatch_' . $request->getFullActionName(), $eventParameters);
  */
-namespace RWCandy\Captcha;
+namespace RWCandy\Captcha\Observer;
 use Magento\Framework\Event\Observer as O;
 use Magento\Framework\Event\ObserverInterface;
 use ReCaptcha\ReCaptcha as RC;
-final class Observer implements ObserverInterface {
+final class CustomerAccountCreatePost implements ObserverInterface {
 	/**
 	 * 2019-11-21
 	 * @override
@@ -21,10 +21,10 @@ final class Observer implements ObserverInterface {
 		try {
 			$rc = new RC('6LeakMMUAAAAANiYs8d8SuADmDXkJ7604rGE43po'); /** @var RC $rc */
 			$rc->setScoreThreshold(0.9);
-			df_assert($rc->verify($res = df_request('rwcCaptcha'))->isSuccess());
-			df_assert(!df_ends_with(df_request('email'), '.ru'));
+			//df_assert($rc->verify($res = df_request('rwcCaptcha'))->isSuccess());
+			/*df_assert(!df_ends_with(df_request('email'), '.ru'));
 			df_assert_lt(40, mb_strlen(df_request('firstname')));
-			df_assert_lt(40, mb_strlen(df_request('lastname')));
+			df_assert_lt(40, mb_strlen(df_request('lastname')));*/
 		}
 		catch (\Exception $e) {
 			df_message_error('reCAPTCHA validation failed.');
---
+++ Observer/CustomerSaveBefore.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * 2019-11-21 «customer_save_before».
+ * @used-by \Magento\Framework\Model\AbstractModel::beforeSave():
+ *	$this->_eventManager->dispatch($this->_eventPrefix . '_save_before', $this->_getEventData());
+ */
+namespace RWCandy\Captcha\Observer;
+use Magento\Customer\Model\Customer as C;
+use Magento\Framework\Event\Observer as O;
+use Magento\Framework\Event\ObserverInterface;
+final class CustomerSaveBefore implements ObserverInterface {
+	/**
+	 * 2019-11-21
+	 * @override
+	 * @see ObserverInterface::execute()
+	 * @see \Itoris\RegFields\Controller\Account\CreatePost::execute()
+	 * @see \Magento\Customer\Model\AccountManagement::createAccount()
+	 * @see \Magento\Framework\Model\AbstractModel::beforeSave()
+	 * @used-by \Magento\Framework\Event\Invoker\InvokerDefault::_callObserverMethod()
+	 * @used-by \Magento\Framework\App\Action\Action::dispatch()
+	 * @param O $o
+	 */
+	function execute(O $o) {
+		try {
+			$c = $o['customer']; /** @var C $c */
+			df_assert(!df_ends_with($c->getEmail(), '.ru'));
+			df_assert_lt(40, mb_strlen($c['firstname']));
+			df_assert_lt(40, mb_strlen($c['lastname']));
+		}
+		catch (\Exception $e) {
+			df_error('The customer is a bot.');
+		}
+	}
+}
